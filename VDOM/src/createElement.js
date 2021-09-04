50ce62bd1d7a25e848d7c943558d20cfbab3c690
chesterchenn <chesterchenn@gmail.com>
2021-09-05 00:28:09 +0800

    feat: 改进createElement函数

--- VDOM/src/createElement.js
+++ VDOM/src/createElement.js
@@ -1,17 +1,43 @@
-export const createElement = function (type, props, child) {
-  if (child === null && child === undefined) return document.createTextNode(type)
-  const element = document.createElement(type);
+// export const createElement = function (type, props, child) {
+//   if (child === null && child === undefined) return document.createTextNode(type)
+//   const element = document.createElement(type);
+//
+//   setProps(element, props);
+//
+//   child.map(createElement).forEach(element.appendChild.bind(ele.type, ele.props, ele.child))
+//
+//   return element;
+// };
 
-  setProps(element, props);
-
-  child.map(createElement).forEach(element.appendChild.bind(ele.type, ele.props, ele.child))
-
-  return element;
+export const createElement = function (type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement
+      ),
+    },
+  };
 };
+
+const createTextElement = function (text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+};
+
+// 自定义的 createTextElement
+// const createTextElement = function (text) {
+//   return document.createTextNode(text);
+// }
 
 const setProps = function (element, props) {
   for (key in props) {
     element.setAttribute(key, props[key]);
   }
 };
-
