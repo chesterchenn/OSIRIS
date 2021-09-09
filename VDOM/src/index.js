51b7007599a51ad8d4ad43ed9525a6a437f91d9f
chesterchenn <chesterchenn@outlook.com>
2021-09-09 10:16:32 +0800

    feat: 移除遍历

--- VDOM/src/index.js
+++ VDOM/src/index.js
@@ -1,4 +1,3 @@
-export { traversalIteration, eachNode } from "./traversal";
 export { recursion } from "./recursion";
 export { createElement } from './createElement';
 export { render } from './render';
