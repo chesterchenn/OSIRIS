93cd5fad0df01031f4d6d06860beaeb48983d602
chesterchenn <chesterchenn@gmail.com>
2021-09-29 22:38:42 +0800

    ci: 修复文件夹问题

--- bin/build.sh
+++ bin/build.sh
@@ -9,16 +9,17 @@ echo "构建目录文件夹: ${BUILD_DIR}"
 remote_branch="gh-pages"
 echo "部署分支：${remote_branch}"
 
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+
 echo "Clone ${remote_branch} from repo ${REMOTE_REPO}"
-git clone --branch $remote_branch $REMOTE_REPO .
+git clone --branch $remote_branch $REMOTE_REPO 
 LOCAL_BRANCH=$remote_branch
 
 echo "本地分支是：${LOCAL_BRANCH}"
 
 npm run build
 echo "构建完成"
-
-cd ${BUILD_DIR}
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
