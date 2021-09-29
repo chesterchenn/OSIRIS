e4ef344d3895d32bcc5a10b3954a912a322a9388
chesterchenn <chesterchenn@outlook.com>
2021-09-29 17:36:05 +0800

    feat: 使用模板生成 html

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,6 +1,21 @@
-require('esbuild').buildSync({
-  entryPoints: ['src/index.tsx'],
-  bundle: true,
-  outdir: 'build',
-  tsconfig: 'tsconfig.json'
-});
+const { htmlPlugin } = require('@craftamap/esbuild-plugin-html');
+
+require('esbuild')
+  .build({
+    entryPoints: ['src/index.tsx'],
+    bundle: true,
+    outdir: 'build',
+    tsconfig: 'tsconfig.json',
+    metafile: true,
+    plugins: [
+      htmlPlugin({
+        files: [
+          {
+            filename: 'index.html',
+            entryPoints: ['src/index.tsx'],
+          },
+        ],
+      }),
+    ],
+  })
+  .catch(() => process.exit(1));
