f7909aa843023c307bbac47498aa75df296dc955
chesterchenn <chesterchenn@gmail.com>
2021-10-06 12:44:22 +0800

    feat: 修改首页

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import  './styles.scss'
+import './styles.scss';
 
-export default function() {
-  return <div className="card">Card</div>
+interface IProps {
+  title: string;
+  link?: string;
+}
+
+export default function (props: IProps) {
+  return (
+    <div className='card'>
+      <div className='card__content'></div>
+      <div className='card__footer'>
+        <a href={props.link}>{props.title}</a>
+      </div>
+    </div>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/card';
+import './index.scss';
 
 const root = document.getElementById('root');
-const ele = <h1><Card /></h1>;
+const ele = (
+  <div className='container'>
+    <Card title='颜色转换' link='' />
+    <Card title='web216颜色' link='' />
+  </div>
+);
 ReactDOM.render(ele, root);
