91f764eada562c4992666943d323b1c3820deb2e
chesterchenn <chesterchenn@outlook.com>
2022-01-07 11:00:51 +0800

    feat: 修改web216颜色的排列

--- pages/web216/index.tsx
+++ pages/web216/index.tsx
@@ -1,12 +1,20 @@
 import styles from './web.module.scss';
 import { Fragment } from 'react';
-import BackToHome from 'components/backToHome'
+import BackToHome from 'components/backToHome';
+import clsx from 'clsx';
 
-function createElement(bg: string) {
+function createElement(bg: string[]) {
+  const showWhite = bg.reduce((prev, val) => {
+    if (['00', '33', '66'].includes(val)) prev++;
+    return prev;
+  }, 0);
+  const backgroundColor = `#${bg.join('')}`;
   const element = (
-    <div>
-      <div className={styles.ele} style={{ backgroundColor: bg }}></div>
-      <div>${bg}</div>
+    <div
+      className={clsx(styles.ele, showWhite >= 3 && styles.whiteText)}
+      style={{ backgroundColor: backgroundColor }}
+    >
+      {backgroundColor}
     </div>
   );
   return element;
@@ -22,7 +30,7 @@ export default function () {
         {colors.map((r: string) =>
           colors.map((g: string) =>
             colors.map((b: string) => (
-              <Fragment key={`${r}${g}${b}`}>{createElement(`#${r}${g}${b}`)}</Fragment>
+              <Fragment key={`${r}${g}${b}`}>{createElement([r, g, b])}</Fragment>
             ))
           )
         )}
