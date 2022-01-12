a0231db9cab4de6c4da4a3cdab75035a8d3f0d70
chesterchenn <chesterchenn@outlook.com>
2022-01-12 18:04:08 +0800

    feat: 修改导航为配置项

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,33 @@
 import Link from 'next/link';
 import Head from 'next/head';
 
+interface Nav {
+  title: string;
+  link: string;
+}
+
 export default function Home() {
+  const navs: Nav[] = [
+    {
+      title: 'Color',
+      link: '/color',
+    },
+    {
+      title: 'Web 216',
+      link: '/web216',
+    },
+  ];
+
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
-      <h2>
-        <Link href='/color'>Color</Link>
-      </h2>
-      <h2>
-        <Link href='/web216'>Web 216</Link>
-      </h2>
+      {navs.map((n) => (
+        <h2 key={n.link}>
+          <Link href={n.link}>{n.title}</Link>
+        </h2>
+      ))}
     </>
   );
 }
