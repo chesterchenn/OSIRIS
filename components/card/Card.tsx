baa2e42eb97444ef6bd612a72fe1f081aea525c9
chesterchenn <chesterchenn@outlook.com>
2022-01-24 18:06:04 +0800

    feat: 补充 card 样式

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,11 +1,19 @@
-import { ReactNode } from 'react';
+import styles from './style.module.scss';
+import clsx from 'clsx';
 
 interface Props {
   children?: React.ReactNode;
+  className?: string;
 }
 
 const Card = (props: Props) => {
-  return <h2>{props.children}</h2>;
+  const { ...rest } = props;
+  return (
+    <div className={clsx(styles.card)} {...rest}>
+      <div className={styles.card__body}></div>
+      <div className={styles.card__footer}>{props.children}</div>
+    </div>
+  );
 };
 
 export default Card;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
 import Link from 'next/link';
-import Head from 'next/head';
 import Card from 'components/card';
+import styles from './home.module.scss';
 import { navs } from './config';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Home</title>
-      </Head>
-      {navs.map((n) => (
-        <Card key={n.link}>
-          <Link href={n.link}>{n.title}</Link>
-        </Card>
-      ))}
+      <div className={styles.home}>
+        {navs.map((n) => (
+          <Card key={n.link}>
+            <Link href={n.link}>{n.title}</Link>
+          </Card>
+        ))}
+      </div>
     </>
   );
 }
