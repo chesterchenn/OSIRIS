370b1fd06c8088dcb8eeab2dbb0e2452b99a6d4b
chesterchenn <chesterchenn@outlook.com>
2022-02-11 09:28:54 +0800

    fix: 修复 Input 组件导出的问题

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import styles from './style.module.scss';
 
 type IProps = React.DetailedHTMLProps<
-React.InputHTMLAttributes<HTMLInputElement>,
-HTMLInputElement
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
 >;
 
-export default const Input = React.forwardRef<HTMLInputElement, IProps>((props, ref) => (
+const Input = React.forwardRef<HTMLInputElement, IProps>((props, ref) => (
   <input autoComplete="off" className={styles.input} {...props} ref={ref} />
 ));
+
+export default Input;
