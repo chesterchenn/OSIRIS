625ac3fb52215f4ca25cbc2707f91f9ef5a23277
chesterchenn <chesterchenn@gmail.com>
2022-11-01 16:15:04 +0800

    perf: ⚡不同的渲染对比

--- pages/million/index.tsx
+++ pages/million/index.tsx
@@ -3,24 +3,46 @@ import Button from 'components/button';
 
 function Million() {
   const total = 1000000;
-  function add(idx: string) {
-    const divElement = document.createElement('div');
+  const once = 200;
+  const counter = total / once;
+  let renderCounter = 1;
+  function add() {
+    const fragment = document.createDocumentFragment();
     const root = document.getElementById('root');
-    const random = Math.floor(Math.random() * total);
-    divElement.innerText = `${idx.padStart(6, '0')}  ${random}`;
-    divElement.setAttribute('style', 'background: #ffe411; margin: 2px;');
-    root?.appendChild(divElement);
-  }
-  function a() {
-    console.time('start');
-    for (let i = 0; i < total; i++) {
-      add(String(i));
+    for (let i = 0; i < once; i++) {
+      const divElement = document.createElement('div');
+      const random = Math.floor(Math.random() * total);
+      const strCount = String(renderCounter);
+      divElement.innerText = `${strCount} ${random}`;
+      divElement.setAttribute('style', 'background: #ffe411; margin: 2px;');
+      fragment.appendChild(divElement);
     }
-    console.timeEnd('start');
+    root?.appendChild(fragment);
+    renderCounter += 1;
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    b();
+  }
+  function addA() {
+    const fragment = document.createDocumentFragment();
+    const root = document.getElementById('root');
+    for (let i = 0; i < total; i++) {
+      const divElement = document.createElement('div');
+      const random = Math.floor(Math.random() * total);
+      divElement.innerText = `${i} ${random}`;
+      divElement.setAttribute('style', 'background: #ffe411; margin: 2px;');
+      fragment.appendChild(divElement);
+    }
+    root?.appendChild(fragment);
+  }
+  function b() {
+    if (renderCounter <= counter) {
+      window.requestAnimationFrame(add);
+    }
   }
   return (
     <>
-      <Button onClick={() => a()}>Button</Button>
+      <Button onClick={() => addA()}>Button1</Button>
+      <Button onClick={() => b()}>Button2</Button>
       <div id="root" />
     </>
   );
