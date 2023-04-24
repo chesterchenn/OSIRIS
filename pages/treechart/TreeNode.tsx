43635dcb785aa2705ae519d95c74cac7dcaa6654
chesterchenn <chesterchenn@gmail.com>
2023-04-25 00:03:08 +0800

    feat: ✨补充母亲节点

--- pages/treechart/TreeNode.tsx
+++ pages/treechart/TreeNode.tsx
@@ -1,23 +1,39 @@
 import React, { Children } from 'react';
 import type { ReactNode } from 'react';
+import clsx from 'clsx';
 import styles from './treeNode.module.scss';
 
 export interface TreeNodeProps {
   label: ReactNode;
+  hasWife?: boolean;
   children?: ReactNode;
 }
 
-function TreeNode({ children = null, label }: TreeNodeProps) {
+interface ICouple {
+  label: ReactNode;
+}
+
+function TreeNode({ children = null, label, hasWife }: TreeNodeProps) {
   return (
-    <li className={styles.node}>
+    <li className={clsx(styles.node)}>
       {label}
-      {Children.count(children) > 0 && <ul className={styles.childrenContainer}>{children}</ul>}
+      {Children.count(children) > 0 && (
+        <ul className={clsx(styles.childrenContainer, { [styles.haswife]: hasWife })}>
+          {children}
+        </ul>
+      )}
     </li>
   );
 }
 
+function Couple({ label }: ICouple) {
+  return <span className={styles.couple}>{label}</span>;
+}
+
 TreeNode.defaultProps = {
   children: null,
+  hasWife: false,
 };
 
 export default TreeNode;
+export { Couple };
--- pages/treechart/index.tsx
+++ pages/treechart/index.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
-import TreeNode from './TreeNode';
+import clsx from 'clsx';
+import TreeNode, {Couple} from './TreeNode';
 import Tree from './Tree';
+import styles from './index.module.scss';
+
+interface ILabel {
+  label: string;
+  hasWife?: boolean;
+}
+
+function Label({ label, hasWife }: ILabel) {
+  return <span className={clsx({[styles.hasWifeLabel]: hasWife})}>{label}</span>;
+}
+
+Label.defaultProps = {
+  hasWife: false,
+};
 
 function TreeChart() {
   return (
-    <Tree label="Root">
+    <Tree label=<Label label='Root' />>
       <TreeNode label="1-1">
-        <TreeNode label="2-1" />
-        <TreeNode label="2-2" />
+        <TreeNode label="2-1">
+          <TreeNode label=<Label label="3-1" /> hasWife>
+            <Couple label="1234" />
+            <TreeNode label="4-1" />
+            <TreeNode label="4-1" />
+          </TreeNode>
+        </TreeNode>
+        <TreeNode label=<Label label="2-2" /> hasWife>
+          <Couple label="123" />
+          <TreeNode label="3-1" />
+          <TreeNode label="3-1" />
+        </TreeNode>
       </TreeNode>
       <TreeNode label="1-2">
-        <TreeNode label="2-3" />
+        <TreeNode label="2-3">
+          <TreeNode label="3-3" />
+        </TreeNode>
       </TreeNode>
     </Tree>
   );
