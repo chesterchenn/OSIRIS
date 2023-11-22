7458123e4599222a6924e97ddd3d245ea13e9531
chesterchenn <chesterchenn@gmail.com>
2023-11-22 23:30:50 +0800

    refactor: 🔥删除不重要的东西

--- config/navs.ts
+++ config/navs.ts
@@ -12,18 +12,6 @@ const navs: Nav[] = [
     title: 'Web 216',
     link: '/web216',
   },
-  {
-    title: 'Foods',
-    link: '/foods',
-  },
-  {
-    title: 'Million',
-    link: '/million',
-  },
-  {
-    title: 'Tree Chart',
-    link: '/treechart',
-  }
 ];
 
 export default navs;
